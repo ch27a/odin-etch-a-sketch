95ce8b4092d9f1728c862776898eed8904f99edb
ch27a <141591933+ch27a@users.noreply.github.com>
2025-01-15 23:31:53 +0100

    Add styling to container, columns and cells

--- createGrid.js
+++ createGrid.js
@@ -1,13 +1,22 @@
 const container = document.querySelector("#container");
+const rowNumber = 16;
+const columnNumber = 16;
 
-for (let i = 0; i < 16; i++)
+const containerHeight = 960;
+const containerWidth = 960;
+container.style.width = String(containerWidth) + "px";
+container.style.height = String(containerHeight) + "px";
+
+for (let i = 0; i < columnNumber; i++)
 {
     const column = document.createElement("div");
     column.classList.add("column")
-    for (let j = 0; j < 16; j++)
+    for (let j = 0; j < rowNumber; j++)
     {
         const cell = document.createElement("div");
         cell.classList.add("cell");
+        cell.style.width = String(containerWidth / columnNumber) + "px";
+        cell.style.height = String(containerHeight / rowNumber) + "px";
         column.appendChild(cell);
     }
     container.appendChild(column);
